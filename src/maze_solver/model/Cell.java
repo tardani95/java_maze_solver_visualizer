9b688fe3b8c5886cadce95e981410629f28bae78
Daniel Tar <tardani95@gmail.com>
2019-05-31 16:12:07 +0200

    new cell class and heuristic enum

---
+++ src/maze_solver/model/Cell.java
@@ -0,0 +1,177 @@
+package maze_solver.model;
+
+import static java.lang.Math.*;
+
+public class Cell {
+    private int x;
+    private int y;
+
+    private boolean explored;
+    private boolean visited;
+    private double cost;
+    private double destination_distance;
+    private CellType type;
+    private Cell parent;
+
+    /*******CONSTRUCTORS*******/
+
+    private Cell(int x, int y, boolean explored, boolean visited,
+                 double cost, double destination_distance,
+                 CellType cellType, Cell parent) {
+        this.x = x;
+        this.y = y;
+        this.explored = explored;
+        this.visited = visited;
+        this.cost = cost;
+        this.destination_distance = destination_distance;
+        this.parent = parent;
+    }
+
+    public Cell(int x, int y, CellType cellType) {
+        this(x, y, false, false,
+                Double.MAX_VALUE, Double.MAX_VALUE,
+                cellType, null);
+    }
+
+    public Cell(int x, int y) {
+        this(x, y, CellType.DEFAULT);
+    }
+
+    /*******GETTERS AND SETTERS*******/
+
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public double getCost() {
+        return cost;
+    }
+
+    public double getDestination_distance() {
+        return destination_distance;
+    }
+
+    public Cell getParent() {
+        return parent;
+    }
+
+    public boolean isExplored() {
+        return explored;
+    }
+
+    public boolean isVisited() {
+        return visited;
+    }
+
+    private boolean isCellType(CellType cellType) {
+        return type.equals(cellType);
+    }
+
+    // TODO: test result
+    public boolean isStart() {
+        return isCellType(CellType.START);
+    }
+
+    // TODO: test result
+    public boolean isDestination() {
+        return isCellType(CellType.DESTINATION);
+    }
+
+    public void setExplored(boolean explored) {
+        this.explored = explored;
+    }
+
+    public void setExplored() {
+        setExplored(true);
+    }
+
+    public void setVisited(boolean visited) {
+        this.visited = visited;
+    }
+
+    public void setVisited() {
+        setVisited(true);
+    }
+
+    public void setCost(double cost) {
+        this.cost = cost;
+    }
+
+    public void setDestination_distance(double destination_distance) {
+        this.destination_distance = destination_distance;
+    }
+
+    private void setType(CellType type) {
+        this.type = type;
+    }
+
+    public void setTypeStart() {
+        setType(CellType.START);
+    }
+
+    public void setTypeDestination() {
+        setType(CellType.DESTINATION);
+    }
+
+    public void setParent(Cell parent) {
+        this.parent = parent;
+    }
+
+
+    /*******FUNCTIONS*******/
+
+
+    public double distanceTo(Cell c, Heuristic h) {
+        switch (h) {
+            case MANHATTAN:
+                return abs(this.x - c.x) + abs(this.y - c.y);
+            case EUCLIDEAN:
+                return sqrt(pow(this.x - c.x, 2) + pow(this.y - c.y, 2));
+            default:
+                System.err.println(h.name() + " distance heuristic not implemented, returning -1.0");
+                return -1.0;
+        }
+    }
+
+    public double distanceTo(Cell c) {
+        return distanceTo(c, Heuristic.EUCLIDEAN);
+    }
+
+    public boolean modifyX(int x){
+        boolean success = false;
+        if(!isCellType(CellType.DEFAULT)){
+            this.x = x;
+            success= true;
+        }
+        return success;
+    }
+    public boolean modifyY(int y){
+        boolean success = false;
+        if(!isCellType(CellType.DEFAULT)){
+            this.y = y;
+            success = true;
+        }
+        return success;
+    }
+
+    public void modifyXY(int x, int y){
+        if(modifyX(x) & modifyY(y)){
+            System.out.println(type.name()+" coordinates modified");
+        }
+    }
+
+
+
+
+    /*******ENUMS*******/
+
+    public enum CellType {
+        DEFAULT,
+        START,
+        DESTINATION
+    }
+}
---
+++ src/maze_solver/model/Heuristic.java
@@ -0,0 +1,6 @@
+package maze_solver.model;
+
+public enum Heuristic {
+    MANHATTAN,
+    EUCLIDEAN
+}
